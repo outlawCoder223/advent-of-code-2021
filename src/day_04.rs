94fe6fd50cc6ff7bfd105f3b631a8e50936e01f7
Rance Campbell <rance@outlawcoding.com>
2021-12-06 10:13:11 -0700

    finish pt 2 day 4

--- src/day_04.rs
+++ src/day_04.rs
@@ -5,10 +5,12 @@ const BOARD_WID: usize = 5;
 const MARKED: i16 = -1;
 const BINGO: i16 = -5;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct BingoBoard {
     board: Vec<Vec<i16>>,
     bingo: bool,
+    counted: bool,
+    score: usize,
 }
 
 impl BingoBoard {
@@ -16,10 +18,15 @@ impl BingoBoard {
         BingoBoard {
             board: vec![vec![0; BOARD_WID]; BOARD_LEN],
             bingo: false,
+            counted: false,
+            score: 0,
         }
     }
 
     fn check_board(&mut self, call_num: i16) -> bool {
+        if self.bingo {
+            return self.bingo;
+        }
         for i in 0..self.board.len() {
             for j in 0..self.board[i].len() {
                 if self.board[i][j] == call_num {
@@ -36,10 +43,14 @@ impl BingoBoard {
             }
         }
 
+        if self.bingo {
+            self.calculate_score(call_num);
+        }
+
         self.bingo
     }
 
-    fn calculate_score(&self, win_num: i16) -> i16 {
+    fn calculate_score(&mut self, win_num: i16) {
         let sum: i16 = self
             .board
             .iter()
@@ -48,7 +59,7 @@ impl BingoBoard {
             .filter(|num| num.is_positive())
             .sum();
 
-        sum * win_num
+        self.score = sum as usize * win_num as usize;
     }
 }
 
@@ -57,80 +68,85 @@ struct BingoData {
     call_nums: Vec<i16>,
     boards: Vec<BingoBoard>,
     winner: bool,
-    score: usize,
+    winning_score: usize,
+    last_score: usize,
 }
+
 impl BingoData {
-    fn new() -> BingoData {
+    fn new(bingo_input: &str) -> BingoData {
+        let mut lines = bingo_input.lines().filter(|&l| !l.is_empty()).into_iter();
+
+        let call_nums = lines
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|n| n.parse::<i16>().unwrap())
+            .collect();
+
+        let mut line_ctr = 0;
+        let mut board_count = 0;
+
+        let mut boards = Vec::new();
+
+        lines.for_each(|line| {
+            if line_ctr == 0 {
+                boards.push(BingoBoard::new());
+            }
+            let nums: Vec<i16> = line
+                .split_whitespace()
+                .filter_map(|n| n.parse::<i16>().ok())
+                .collect();
+
+            boards[board_count].board[line_ctr] = nums;
+            line_ctr += 1;
+
+            if line_ctr >= 5 {
+                line_ctr = 0;
+                board_count += 1;
+            }
+        });
+
         BingoData {
-            call_nums: Vec::new(),
-            boards: Vec::new(),
+            call_nums,
+            boards,
             winner: false,
-            score: 0,
+            winning_score: 0,
+            last_score: 0,
         }
     }
 
     fn play(&mut self) {
-        let mut call_nums = self.call_nums.iter();
+        let call_nums = self.call_nums.iter();
 
-        while !self.winner {
-            let pick = call_nums.next();
+        let mut winning_order: Vec<BingoBoard> = Vec::new();
 
-            match pick {
-                None => {
-                    println!("No one won?");
-                    break;
-                }
-                Some(n) => {
-                    for board in self.boards.iter_mut() {
-                        if board.check_board(*n) {
-                            self.winner = true;
-                            self.score = board.calculate_score(*n) as usize;
-                            println!("{:?}", board.board);
-                        }
+        for num in call_nums {
+            for board in self.boards.iter_mut() {
+                if board.check_board(*num) {
+                    if !board.counted {
+                        winning_order.push(board.clone());
+                        board.counted = true;
                     }
+                } else {
+                    continue;
                 }
             }
         }
-        if self.winner {
-            println!("Winning Score: {}", self.score);
-        }
+        let winning_board = winning_order.first().unwrap();
+        self.winning_score = winning_board.score;
+        let last_board = winning_order.last().unwrap();
+        self.last_score = last_board.score;
+
+        println!("Winning Score: {}", self.winning_score);
+        println!("Last Score: {}", self.last_score);
     }
 }
 
 pub fn execute() {
     println!("---------DAY 4--------");
-    let input = fs::read_to_string("input/input_04.txt").expect("Unable to open file");
-    let mut lines = input.lines().filter(|&l| !l.is_empty()).into_iter();
+    let bingo_input = fs::read_to_string("input/input_04.txt").expect("Unable to open file");
 
-    let mut bingo_data = BingoData::new();
-
-    bingo_data.call_nums = lines
-        .next()
-        .unwrap()
-        .split(",")
-        .map(|n| n.parse::<i16>().unwrap())
-        .collect();
-
-    let mut line_ctr = 0;
-    let mut board_count = 0;
-
-    lines.for_each(|line| {
-        if line_ctr == 0 {
-            bingo_data.boards.push(BingoBoard::new());
-        }
-        let nums: Vec<i16> = line
-            .split_whitespace()
-            .filter_map(|n| n.parse::<i16>().ok())
-            .collect();
-
-        bingo_data.boards[board_count].board[line_ctr] = nums;
-        line_ctr += 1;
-        if line_ctr >= 5 {
-            line_ctr = 0;
-            board_count += 1;
-        }
-    });
+    let mut bingo_data = BingoData::new(bingo_input.as_str());
 
     bingo_data.play();
-    // println!("{:?}", bingo_data.boards);
 }
